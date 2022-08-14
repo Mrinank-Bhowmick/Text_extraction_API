741036605a53ec76a8799a8bf6c59a4170bcfef7
EFFLUX <bhowmickmrinank@gmail.com>
2022-08-14 18:24:50 +0530

    Added runtime.txt

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from importlib.resources import path
 import pathlib
-from re import template
 from fastapi import FastAPI,Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
